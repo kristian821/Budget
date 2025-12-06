06d1e7775f5086f5e158395aa3fa2aee2cb08920
Kristian Mitchell <56667566+kristian821@users.noreply.github.com>
2025-12-05 22:20:37 -0800

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Kristian Mitchell on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
