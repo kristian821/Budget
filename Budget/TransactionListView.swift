6877d360487f80787e32b8850e6fe26247ab43cb
Kristian Mitchell <kmitch79@mail.ccsf.edu>
2025-12-08 21:56:05 -0800

    Adding TransactionListView

---
+++ Budget/TransactionListView.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionListView.swift
+//  Budget
+//
+//  Created by Kristian Mitchell on 12/7/25.
+//
+
+import SwiftUI
+import SwiftData
+
+
+struct TransactionListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionListView()
+}
